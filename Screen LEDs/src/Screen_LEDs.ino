aa199fdcacbc3a414891c442fbfc54d6a4a588b7
InvisiBug <matthewjkavanagh@gmail.com>
2022-01-12 22:33:53 +0000

    Added screen LEDs

---
+++ Screen LEDs/src/Screen_LEDs.ino
@@ -0,0 +1,238 @@
+////////////////////////////////////////////////////////////////////////
+//  Matthew Kavanagh 
+// 
+//  Kavanet 
+//  Table Lamp.ino
+//  2019
+//  *********
+//
+//  Versions
+//  V1 - Project Start - 06/05/2019
+
+/*
+ LEDs
+ Total -------- 136
+ Top ---------- 49
+ Left --------- 20 
+ Right -------- 20
+ Bottom Left -- 23
+ Bottom Right - 24
+
+ Prismatik Settings
+ Top ---------- 49
+ Sides -------- 20
+ Bottom ------- 57
+ Thickness ---- 5%
+ Stand Width -- 0%
+ Start Offset - 1
+
+*/
+
+////////////////////////////////////////////////////////////////////////
+//
+//  ######                                      
+//  #     # # #    #  ####  #    # #####  ####  
+//  #     # # ##   # #    # #    #   #   #      
+//  ######  # # #  # #    # #    #   #    ####  
+//  #       # #  # # #    # #    #   #        # 
+//  #       # #   ## #    # #    #   #   #    # 
+//  #       # #    #  ####   ####    #    ####  
+//
+////////////////////////////////////////////////////////////////////////
+// Board
+// Use Wemos D1 R2 & Mini
+
+
+////////////////////////////////////////////////////////////////////////
+//
+//  ###                                                  
+//   #  #    #  ####  #      #    # #####  ######  ####  
+//   #  ##   # #    # #      #    # #    # #      #      
+//   #  # #  # #      #      #    # #    # #####   ####  
+//   #  #  # # #      #      #    # #    # #           # 
+//   #  #   ## #    # #      #    # #    # #      #    # 
+//  ### #    #  ####  ######  ####  #####  ######  ####  
+//
+////////////////////////////////////////////////////////////////////////
+#include <PubSubClient.h>     // MQTT
+#include <ESP8266WiFi.h>      // Wifi 
+#include <ArduinoOTA.h>       // OTA
+#include <Streaming.h>        // Serial printouts
+#include <NTPClient.h>        // Time
+#include <ArduinoJson.h>      // Json Library
+#include <string.h>
+
+#include <FastLED.h>
+
+// Effects
+// #include "Fire.h"
+#include "Rainbow.h"
+#include "Crisscross.h"
+#include "ColourFade.h"
+#include "ColourCycle.h"
+// #include "ChangeBrightness.h"
+
+////////////////////////////////////////////////////////////////////////
+//
+//  ######                                                        
+//  #     # ###### ###### # #    # # ##### #  ####  #    #  ####  
+//  #     # #      #      # ##   # #   #   # #    # ##   # #      
+//  #     # #####  #####  # # #  # #   #   # #    # # #  #  ####  
+//  #     # #      #      # #  # # #   #   # #    # #  # #      # 
+//  #     # #      #      # #   ## #   #   # #    # #   ## #    # 
+//  ######  ###### #      # #    # #   #   #  ####  #    #  #### 
+//
+////////////////////////////////////////////////////////////////////////
+// Physical I/O
+#define totalLEDs 136
+#define connectionLED D4
+#define LEDdata    D5
+#define ambientPin D6
+
+// Time 
+#define NTP_ADDRESS  "uk.pool.ntp.org"  // Uk time server
+#define NTP_OFFSET   60 * 60            // In seconds
+#define NTP_INTERVAL 60 * 1000          // In miliseconds
+
+// MQTT
+#define mqttLen 50
+#define qos 0
+
+#define ON  LOW
+#define OFF HIGH
+
+#define colour color // Means we can spell it properly
+
+#define serialRate 115200  // Standard
+
+////////////////////////////////////////////////////////////////////////
+//
+//  #     #                                                  
+//  #     #   ##   #####  #####  #    #   ##   #####  ###### 
+//  #     #  #  #  #    # #    # #    #  #  #  #    # #      
+//  ####### #    # #    # #    # #    # #    # #    # #####  
+//  #     # ###### #####  #    # # ## # ###### #####  #      
+//  #     # #    # #   #  #    # ##  ## #    # #   #  #      
+//  #     # #    # #    # #####  #    # #    # #    # ######
+//
+////////////////////////////////////////////////////////////////////////
+// MQTT CLient
+WiFiClient espClient;
+PubSubClient mqtt(espClient);
+
+// Time Client
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
+
+// LED ring
+CRGB currentLED[totalLEDs];
+
+// Effects
+Rainbow         rainbow(totalLEDs, currentLED, 20);
+ColourFade   colourFade(totalLEDs, currentLED, 20);
+Crisscross   crissCross(totalLEDs, currentLED, 20);
+ColourCycle colourCycle(totalLEDs, currentLED, 20);
+
+////////////////////////////////////////////////////////////////////////
+//
+//  #     #                                                    
+//  #     #   ##   #####  #   ##   #####  #      ######  ####  
+//  #     #  #  #  #    # #  #  #  #    # #      #      #      
+//  #     # #    # #    # # #    # #####  #      #####   ####  
+//   #   #  ###### #####  # ###### #    # #      #           # 
+//    # #   #    # #   #  # #    # #    # #      #      #    # 
+//     #    #    # #    # # #    # #####  ###### ######  #### 
+//
+////////////////////////////////////////////////////////////////////////
+char* wifiSsid     = "I Don't Mind";
+char* wifiPassword = "Have2Biscuits";
+
+char* nodeName     = "Screen LEDs";
+char* nodePassword = "crm0xhvsmn";
+
+const char* controlTopic = "Screen LEDs Control";
+
+char* disconnectMsg = "Screen LEDs Disconnected";
+
+char* mqttServerIP = "192.168.1.46";
+
+char msg[mqttLen]; // Buffer to store the MQTT messages
+
+int mode = 0;
+
+
+int LEDBrightness = 10; // As a percentage (saved as a dynamic variable to let us change later)
+
+long interval = (5 * 1000); // Update every 5 seconds
+unsigned long previousMillis = 0;
+long connectionTimeout = (2 * 1000);
+long lastWiFiReconnectAttempt = 0;
+long lastMQTTReconnectAttempt = 0;
+bool WiFiConnected = false;
+
+
+int red, green, blue;
+
+////////////////////////////////////////////////////////////////////////
+//
+//  ######                                                #####                                          
+//  #     # #####   ####   ####  #####    ##   #    #    #     # #####   ##   #####  ##### #    # #####  
+//  #     # #    # #    # #    # #    #  #  #  ##  ##    #         #    #  #  #    #   #   #    # #    # 
+//  ######  #    # #    # #      #    # #    # # ## #     #####    #   #    # #    #   #   #    # #    # 
+//  #       #####  #    # #  ### #####  ###### #    #          #   #   ###### #####    #   #    # #####  
+//  #       #   #  #    # #    # #   #  #    # #    #    #     #   #   #    # #   #    #   #    # #      
+//  #       #    #  ####   ####  #    # #    # #    #     #####    #   #    # #    #   #    ####  #      
+//
+////////////////////////////////////////////////////////////////////////
+void setup()
+{  
+  Serial.begin(serialRate);
+  
+  Serial << "\n| " << nodeName << " |" << endl;
+
+  pinMode(connectionLED, OUTPUT);
+  digitalWrite(connectionLED, ON);  // Should be on (turned off for testing)
+
+  pinMode(ambientPin, OUTPUT);
+  digitalWrite(ambientPin, true);
+
+  FastLED.addLeds<WS2812, LEDdata, GRB>(currentLED, totalLEDs);
+  FastLED.setBrightness( LEDBrightness * 2.55);
+    
+  startWifi();
+  startMQTT();
+  startOTA();
+  // startTime();
+}
+ 
+///////////////////////////////////////////////////////////////////////
+//
+//  #     #                    ######                                     
+//  ##   ##   ##   # #    #    #     # #####   ####   ####  #####    ##   #    # 
+//  # # # #  #  #  # ##   #    #     # #    # #    # #    # #    #  #  #  ##  ## 
+//  #  #  # #    # # # #  #    ######  #    # #    # #      #    # #    # # ## # 
+//  #     # ###### # #  # #    #       #####  #    # #  ### #####  ###### #    # 
+//  #     # #    # # #   ##    #       #   #  #    # #    # #   #  #    # #    # 
+//  #     # #    # # #    #    #       #    #  ####   ####  #    # #    # #    # 
+//
+///////////////////////////////////////////////////////////////////////
+void loop(void)
+{
+  // rainbow.run();
+  // colourFade.run();
+
+  if(mode == 2) rainbow.run();
+  if(mode == 3) colourFade.run();
+
+  handleWiFi();
+  handleMQTT();
+  ArduinoOTA.handle();
+
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+
+    publishAll();
+  }
+}
